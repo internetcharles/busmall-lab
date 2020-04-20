377bb426af919e65e2795495d69db5d69c683d94
internetcharles <charlesodellsmith@gmail.com>
2020-04-19 19:24:19 -0700

    choice counter initializes and counts down, products disappear and reappear on button press

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ import { generateThreeRandom } from './random-images.js';
 import { createAllProducts } from './create-products.js';
 
 const selectionContainer = document.getElementById('selection-container');
+const submitButton = document.getElementById('submit-button');
 
 const randomProducts = generateThreeRandom(products);
 createAllProducts(randomProducts, selectionContainer);
+
+let choiceCounter = 0;
+
+submitButton.addEventListener('click', () => {
+    if (choiceCounter > 25) {
+        submitButton.disabled = true;
+    }
+
+    // replacement of inputs
+
+    while (selectionContainer.firstChild) {
+        selectionContainer.removeChild(selectionContainer.firstChild);
+    }
+
+    const randomProducts = generateThreeRandom(products);
+    createAllProducts(randomProducts, selectionContainer);
+
+    choiceCounter++;
+} )
